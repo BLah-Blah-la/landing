b36eb1881f3375c672c528aec50403467ad0577e
Roman <kuzmin.sto@yandex.ru>
2018-04-05 15:00:58 +0500

    wewq

--- find/Finder.php
+++ find/Finder.php
@@ -5,6 +5,7 @@ use vendor\landing\partner\models\Logo;
 use vendor\landing\partner\models\Advantages;
 use vendor\landing\partner\models\Contacts;
 use vendor\landing\partner\models\Steps;
+use vendor\landing\partner\models\PriceList;
 
 use yii\db\ActiveRecord;
 
@@ -31,6 +32,12 @@ class Finder extends ActiveRecord
 	public function findSteps(){
 		
 		return Steps::find()->all();
+		
+	}
+	
+	public function findPriceList(){
+		
+		return PriceList::find()->all();
 		
 	}
 }
